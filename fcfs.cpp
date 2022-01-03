8d3f0f41a1f9e03c156df32c320c5773a0a98438
Debasmit <debasmitbiswal@outlook.com>
2022-01-03 15:17:17 +0530

    Update fcfs.cpp

--- fcfs.cpp
+++ fcfs.cpp
@@ -8,6 +8,12 @@ void findWaitingTime(int processes[],int n,int bt[],int wt[]){
     wt[i]=bt[i-1]+wt[i-1];
 }
 
+void findTurnAroundTime( int processes[], int n, int bt[], int wt[], int tat[])
+{
+    for (int i = 0; i < n ; i++)
+        tat[i] = bt[i] + wt[i];
+}
+
 void findavgTime(int processes[],int n,int bt[]){
     int wt[n],tat[n],total_wt=0,total_tat=0;
     findWaitingTime(processes,n,bt,wt);
